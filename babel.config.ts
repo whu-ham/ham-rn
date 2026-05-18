7cc9eab86c75fcdc749843322e4a7f76477d3a34
orangeboyChen <chenenhan@qq.com>
2026-05-18 13:17:50 +0800

    feat: upgrade hot-updater to 0.31.0
    - Upgrade @hot-updater/react-native from 0.25.7 to 0.31.0
    - Upgrade @hot-updater/bare from 0.25.7 to 0.31.0
    - Upgrade @hot-updater/cloudflare from 0.25.7 to 0.31.0
    - Upgrade hot-updater CLI from 0.25.7 to 0.31.0
    - Remove deprecated 'hot-updater/babel-plugin' (no longer exported in 0.31.0)
    - Add .hot-updater/log to .gitignore

--- babel.config.ts
+++ babel.config.ts
@@ -4,7 +4,6 @@ const config: TransformOptions = {
   presets: ['@babel/preset-typescript', 'module:@react-native/babel-preset'],
   plugins: [
     '@babel/plugin-transform-export-namespace-from',
-    'hot-updater/babel-plugin',
     [
       'module-resolver',
       {
